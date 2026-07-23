c3198d0693df52f74ba30322b85c5248e050687f
MikeyOgada <mikeogada254@gmail.com>
2026-07-23 07:44:16 +0100

    splunk changes

--- adapters/splunk.py
+++ adapters/splunk.py
@@ -1,32 +1,48 @@
 import json
+import requests
 
 from config.settings import (
+    SPLUNK_HEC_URL,
+    SPLUNK_HEC_TOKEN,
     SPLUNK_INDEX,
     SPLUNK_SOURCE,
     SPLUNK_SOURCETYPE,
 )
 
+
+def build_headers():
+    return {
+        "Authorization": f"Splunk {SPLUNK_HEC_TOKEN}",
+        "Content-Type": "application/json",
+    }
+
+
 def build_payload(event):
-    """
-    Build the payload for Splunk HEC.
-    """
-    payload = {
+    return {
         "index": SPLUNK_INDEX,
         "source": SPLUNK_SOURCE,
         "sourcetype": SPLUNK_SOURCETYPE,
         "host": event.repo,
-        "event": event.__dict__
+        "event": event.__dict__,
     }
-    
-    return payload
-#payload etl to fit splunk hec format. The build_payload function takes a SupplyChainEvent object as input and returns a dictionary that represents the payload to be sent to Splunk HEC. The payload includes the timestamp, host (repository), source, sourcetype, and the event data itself. 
 
 
 def send_to_splunk(event):
 
-    print("Sending event to Splunk")
-
     payload = build_payload(event)
 
-    print(json.dumps(payload, indent=2)) #print the payload as a JSON object
+    print(json.dumps(payload, indent=2))
 
+    response = requests.post(
+        SPLUNK_HEC_URL,
+        headers=build_headers(),
+        json=payload,
+        verify=False,
+    )
+
+    if response.status_code == 200:
+        print("Event sent to Splunk successfully")
+    else:
+        print(f"Failed to send event: {response.text}")
+
+    return response
