0b795ecc9639c50be2ab9104147856133df9fb2a
MikeyOgada <mikeogada254@gmail.com>
2026-07-23 07:45:02 +0100

    setting changes

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,14 @@
 import os
 
+
+
 # Splunk HEC connection
-SPLUNK_HEC_URL = os.getenv("SPLUNK_HEC_URL")
-SPLUNK_HEC_TOKEN = os.getenv("SPLUNK_HEC_TOKEN")
+SPLUNK_HEC_URL = os.getenv(
+    "SPLUNK_HEC_URL",
+    "https://localhost:8088/services/collector/event"
+)
+
+SPLUNK_HEC_TOKEN = os.getenv("SPLUNK_HEC_TOKEN" "55ec52a9-9f4c-424f-9061-ce390a859a27")#HEC token for Splunk HEC authentication. This token is used to authenticate the requests sent to the Splunk HEC endpoint. It is important to keep this token secure and not expose it in public repositories or logs.
 
 # Event metadata
 SPLUNK_INDEX = os.getenv("SPLUNK_INDEX", "supply_chain")
